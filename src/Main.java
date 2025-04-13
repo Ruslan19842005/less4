3c0b9b71c67424b913051ba01ef89ebb42bdda6d
yamal <yamal_07@mail.ru>
2025-04-13 11:53:12 +0900

    Zadanie- 2

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,10 @@ public class Main {
     public static void main(String[] args) {
         int age = 18 ;
         System.out.println("Если человеку 18 и больше лет он совершеннолетний");
-        if (age <= 18);
+        if (age < 18);
         System.out.println("Человеку нет 18 нужно подождать");
+
+
 
 
 
