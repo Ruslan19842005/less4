9a9755482fbffa491b451779aec3e589fc07c5a9
yamal <yamal_07@mail.ru>
2025-04-18 18:47:26 +0900

    Zadanie- 5

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        int speed = 40; //
-        if (speed > 60) {
-            System.out.println("Если скорость " + speed + ", то придется заплатить штраф");
+        int age = 18;
+
+        if (age >= 2 && age <= 6) {
+            System.out.println("Если возраст человека равен " + age + ", то ему нужно ходить в детский сад.");
+        } else if (age >= 7 && age <= 17) {
+            System.out.println("Если возраст человека равен " + age + ", то ему нужно ходить в школу.");
+        } else if (age >= 18 && age <= 24) {
+            System.out.println("Если возраст человека равен " + age + ", то его место в университете.");
         } else {
-            System.out.println("Если скорость " + speed + ", то можно ездить спокойно");
+            System.out.println("Если возраст человека равен " + age + ", то ему пора ходить на работу.");
         }}}
 
 
