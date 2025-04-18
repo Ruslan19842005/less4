d29fdbdbc2eb46b1dba0a25b58a5a04bc39de0b3
yamal <yamal_07@mail.ru>
2025-04-18 17:54:27 +0900

    Zadanie- 3 Почему тут ошибка?

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        int age = 18 ;
+        int age = 18;
         System.out.println("Если человеку 18 и больше лет он совершеннолетний");
-        if (age < 18);
+        if (age < 18) ;
         System.out.println("Человеку нет 18 нужно подождать");
 
 
+        int temperature = 3;
+        if (temperature <= 5){
+            System.out.println("На улице " + temperature + " градусов, нужно надеть шапку.");
+        } else {
+            System.out.println("На улице " + temperature + " градусов, можно идти без шапки.");
+        }
 
 
 
@@ -13,6 +19,20 @@ public class Main {
 
 
 
-    }
 
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
