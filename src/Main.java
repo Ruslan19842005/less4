a9d385c47006b904d1ac0cbddbd97ba60a266316
yamal <yamal_07@mail.ru>
2025-04-19 21:38:33 +0900

    Zadanie- 6

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,22 @@
 public class Main {
     public static void main(String[] args) {
-        int age = 18;
 
-        if (age >= 2 && age <= 6) {
-            System.out.println("Если возраст человека равен " + age + ", то ему нужно ходить в детский сад.");
-        } else if (age >= 7 && age <= 17) {
-            System.out.println("Если возраст человека равен " + age + ", то ему нужно ходить в школу.");
-        } else if (age >= 18 && age <= 24) {
-            System.out.println("Если возраст человека равен " + age + ", то его место в университете.");
+        int age = 10;
+        boolean hasAdult = true;
+
+        if (age < 5) {
+            System.out.println("Если возраст ребенка равен " + age + ", то ему нельзя кататься на аттракционе");
+        } else if (age < 14) {
+            if (hasAdult) {
+                System.out.println("Если возраст ребенка равен " + age + ", то ему можно кататься на аттракционе в сопровождении взрослого");
+            } else {
+                System.out.println("Если возраст ребенка равен " + age + ", то ему нельзя кататься на аттракционе");
+            }
         } else {
-            System.out.println("Если возраст человека равен " + age + ", то ему пора ходить на работу.");
+            System.out.println("Если возраст ребенка равен " + age + ", то ему можно кататься на аттракционе без сопровождения взрослого");
         }}}
+
+
 
 
 
